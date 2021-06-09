d9cd7b36676a8ff36e2baaba36b9122074026734
ilexwg <ilexwg@gmail.com>
2021-06-10 01:25:24 +0800

    feat: add test & lint

--- meadowlark.js
+++ meadowlark.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
 
-const fortune = require('./lib/fortune');
+const handlers = require('./lib/handlers');
 
 const app = express();
 
@@ -14,26 +14,21 @@ app.use(express.static(__dirname + '/public'));
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.render('home'));
+app.get('/', handlers.home);
 
-app.get('/about', (req, res) => {
-  res.render('about', { fortune: fortune.getFortune() });
-});
+app.get('/about', handlers.about);
 
 // custom 404 page
-app.use((req, res) => {
-  res.status(404);
-  res.render('404');
-});
+app.use(handlers.notFound);
 
 // custom 500 page
-app.use((err, req, res, next) => {
-  console.error(err.message);
-  res.status(500);
-  res.render('500');
-});
+app.use(handlers.serverError);
 
-app.listen(port, () => {
-  console.log(`Express started on http://localhost:${port}`);
-  console.log('Press Ctrl-C to terminate.');
-});
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Express started on http://localhost:${port}`);
+    console.log('Press Ctrl-C to terminate.');
+  });
+} else {
+  module.exports = app;
+}
