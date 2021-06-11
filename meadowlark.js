ea71219928bea72d6ad62474e3c262548affeb35
ilexwg <ilexwg@gmail.com>
2021-06-11 17:47:10 +0800

    temp commit

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 const handlers = require('./lib/handlers');
 const weatherMiddleware = require('./lib/middleware/weather');
@@ -20,6 +21,9 @@ app.engine('handlebars', expressHandlebars({
 }));
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use(express.static(__dirname + '/public'));
 
 app.use(weatherMiddleware);
@@ -27,13 +31,18 @@ app.use(weatherMiddleware);
 const port = process.env.PORT || 3000;
 
 app.get('/', handlers.home);
-
 app.get('/about', handlers.about);
 
-// custom 404 page
-app.use(handlers.notFound);
+// handlers for browser-based form submission
+app.get('/newsletter-signup', handlers.newsletterSignup);
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess);
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou);
 
-// custom 500 page
+// handlers for fetch/JSON form submission
+app.get('/newsletter', handlers.newsletter);
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup);
+
+app.use(handlers.notFound);
 app.use(handlers.serverError);
 
 if (require.main === module) {
