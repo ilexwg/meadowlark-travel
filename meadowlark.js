90bb0ae437d2ffa4bd26bd421d5f41b0635e3910
ilexwg <ilexwg@gmail.com>
2021-06-13 16:27:13 +0800

    feat: handle form

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
+const multiparty = require('multiparty');
 
 const handlers = require('./lib/handlers');
 const weatherMiddleware = require('./lib/middleware/weather');
@@ -41,6 +42,30 @@ app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou);
 // handlers for fetch/JSON form submission
 app.get('/newsletter', handlers.newsletter);
 app.post('/api/newsletter-signup', handlers.api.newsletterSignup);
+
+// vacation photo contest
+app.get('/contest/vacation-photo', handlers.vacationPhotoContest);
+app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+  const form = new multiparty.Form();
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(500).send({ error: err.message });
+    }
+    handlers.vacationPhotoContestProcess(req, res, fields, files);
+  });
+});
+app.get('/contest/vacation-photo-thank-you', handlers.vacationPhotoContestProcessThankYou);
+app.get('/contest/vacation-photo-ajax', handlers.vacationPhotoContestAjax);
+app.post('/api/vacation-photo-contest/:year/:month', (req, res) => {
+  const form = new multiparty.Form();
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return handlers.api.vacationPhotoContestError(req, res, err.message);
+    }
+    handlers.api.vacationPhotoContest(req, res, fields, files);
+  });
+});
 
 app.use(handlers.notFound);
 app.use(handlers.serverError);
